137f42030021d7fe583fceec8c7cf18468a5cb95
Tusti-Gupta <86391913+Tusti-Gupta@users.noreply.github.com>
2023-07-29 00:27:33 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+camera = document.getElementById("camera");
+
+Webcam.attach("#camera");
+
+function take_snapshot() {
+    Webcam.snap(function (data_uri) {
+        document.getElementById("result").innerHTML = '<img id="captured_image" src="'+data_uri+'"/>';
+    });
+}
+
+console.log('ml5 version', ml5.version);
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/figjRGLwH/model.json',modelLoaded);
+
+function modelLoaded() {
+    console.log("Model Loaded!");
+}
+
+function check() {
+    img = document.getElementById("captured_image");
+    classifier.classify(img, gotResult);
+}
+
+function speak() {
+    var synth = window.speechSynthesis;
+    speak_data_1 = "Are you showing" + prediction_1;
+    speak_data_2 = "or" + prediction_2 + "?";
+    var utterThis = new SpeechSynthesisUtterance(speak_data_1 + speak_data_2);
+    synth.speak(utterThis);
+}
